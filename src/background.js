ffd6cab28e5c14e0ca84bdf22c0028835c2e5d3e
Sudip Pathak <sudippathak10000@gmail.com>
2024-08-24 18:00:19 +0545

    added manifest.json file for manifestation task and background.js and youtube.js for the functionality.

---
+++ src/background.js
@@ -0,0 +1,8 @@
+chrome.runtime.onMessage.addListener((request, sender, callback) => {
+  let filename = request.filename.replace(/\?|:|"|~|<|>|\*|\|/g, "");
+  let url = request.url;
+  chrome.downloads.download({
+    filename,
+    url,
+  });
+});
